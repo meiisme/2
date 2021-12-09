9e580a06a92585bc3903059eb7cc1fabf7eb7b8b
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-09 21:10:11 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,7 +5,7 @@ gas=st.number_input('油量的資料收集:油箱滿是10格 =>')
 speed=st.number_input('車速的資料收集:限速100 =>')
 temp=st.number_input('溫度的資料收集:限溫30 =>')
 轉速=st.number_input('轉速資料的收集:限轉12000 =>')
-confirm_input=st.bottom('輸入確認')
+confirm_input=st.buttom('輸入確認')
 if confirm_input: 
     if gas <= car_simulation.get('gas_warning'):
       st.write('油箱只剩', gas, '格! 準備加油!!')
