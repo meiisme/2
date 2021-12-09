2472259059c9d979ca5dc6bad9f53de6e6a29f13
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-09 21:03:49 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
 
 car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, '轉速':12000}
+
 gas=st.number_input('油量的資料收集:油箱滿是10格 =>')
 speed=st.number_input('車速的資料收集:限速100 =>')
 temp=st.number_input('溫度的資料收集:限溫30 =>')
@@ -7,9 +8,9 @@ temp=st.number_input('溫度的資料收集:限溫30 =>')
 confirm_input=st.bottom('輸入確認')
 
 if gas <= car_simulation.get('gas_warning'):
-  st.write('油箱只剩', gas, '格! 準備加油!!')
+    st.write('油箱只剩', gas, '格! 準備加油!!')
 else:
-   st.write('油箱還剩', gas, '格。')
+    st.write('油箱還剩', gas, '格。')
 if speed>=car_simulation.get('speed_limit'):
     st.write('即將超速')
 else:
@@ -17,8 +18,8 @@ else:
 if temp>=car_simulation.get('temp_warning'):
     st.write('過熱')
 else:
-   st.write('正常')
+    st.write('正常')
 if 轉速>=car_simulation.get('轉速'):
-     st.write('即將超過轉速')
+    st.write('即將超過轉速')
 else:
-     st.write('正常')
+    st.write('正常')
