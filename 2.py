8d3030a84abb3c999bf5001f12744ea29c4a379d
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-09 21:01:33 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
-st.title('vamos')
+
 car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, '轉速':12000}
 gas = st.number_input('油量的資料收集:油箱滿是10格 =>')
 speed = st.number_input('車速的資料收集:限速100 =>')
