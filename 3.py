d4ea9eb4d67c8c23fb3caddc479b0af21a2d248e
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-09 21:21:27 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,19 @@
+'''組態檔'''
+import os
+
+
+'''螢幕大小'''
+SCREENSIZE = (800, 625)
+'''遊戲素材'''
+BGMPATH = os.path.join(os.getcwd(), 'resources/audios/bgm.mp3')
+HEROPICPATH = os.path.join(os.getcwd(), 'resources/images/hero.png')
+'''FPS'''
+FPS = 20
+'''塊大小'''
+BLOCKSIZE = 15
+MAZESIZE = (35, 50) # num_rows * num_cols
+BORDERSIZE = (25, 50) # 25 * 2 + 50 * 15 = 800, 50 * 2 + 35 * 15 = 625
+
 import cfg
 import sys
 import pygame
